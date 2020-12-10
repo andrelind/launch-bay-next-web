9b495a7b4fd1dc28691884c9bd1cc0046a059844
André Lind <andre@lindapp.se>
2020-12-10 08:28:09 +0100

    Fix patch

--- pages/api/graphql/index.ts
+++ pages/api/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloServer } from "apollo-server-micro";
 
-const resolvers = require("../../schema/resolvers");
-const typeDefs = require("../../schema/typeDefs").default;
+const resolvers = require("../../../schema/resolvers");
+const typeDefs = require("../../../schema/typeDefs").default;
 
 console.log({ resolvers, typeDefs });
 
