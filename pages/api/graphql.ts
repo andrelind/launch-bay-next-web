82454ce1224f4ece464261b92f68df8ce00dc76e
André Lind <andre@lindapp.se>
2020-12-18 09:58:36 +0100

    Try new

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,44 +1,11 @@
-import { ApolloServer } from 'apollo-server-express';
-import cors from 'cors';
-import express from 'express';
-import { makeExecutableSchema } from 'graphql-tools';
-import mongoConnection from '../../schema/connection';
-import corsOptions from '../../schema/cors';
+import { ApolloServer } from 'apollo-server-micro';
 import resolvers from '../../schema/resolvers';
 import typeDefs from '../../schema/typeDefs';
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
+console.log({ resolvers, typeDefs });
 
-// Define the server (using Express for easier middleware usage)
-const server = new ApolloServer({
-  schema,
-  introspection: process.env.NODE_ENV !== 'production',
-  playground:
-    process.env.NODE_ENV !== 'production'
-      ? {
-          settings: {
-            'request.credentials': 'include',
-          },
-        }
-      : false,
-});
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
-const app = express();
+export const config = { api: { bodyParser: true } };
 
-app.set('trust proxy', true);
-
-const gqlPath = '/api/graphql';
-// setup cors
-app.use(gqlPath, cors(corsOptions));
-// init the db
-app.use(gqlPath, mongoConnection());
-
-server.applyMiddleware({ app, path: '/api/graphql' });
-
-export default app;
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+export default apolloServer.createHandler({ path: '/api/graphql' });
